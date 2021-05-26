2c8ad0ee84614bcde97a2f143b21b8337b153b9e
Priyadharsini Ramu <Priyadharsini.Ramu@gds.ey.com>
2021-05-26 17:22:57 +0530

    Added HTML summary

--- sample.js
+++ sample.js
@@ -1,13 +1,14 @@
 import http from 'k6/http';
-
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 export let options = {
   discardResponseBodies: true,
   scenarios: {
     sample: {
       executor: 'constant-vus',
       exec: 'users',
-      vus: 15,
-      duration: '15s',
+      vus: 5,
+      duration: '5s',
     },
   },
 };
@@ -17,3 +18,14 @@ export function users() {
   http.get(url, { tags: { function: 'echo' } });
 }
 
+export function handleSummary(data) {
+  console.log('Preparing the end-of-test summary...');
+  let resp = http.post('https://httpbin.test.k6.io/anything', JSON.stringify(data));
+    if (resp.status != 200) {
+        console.error('Could not send summary, got status ' + resp.status);
+    }
+  return {
+        'stdout': textSummary(data, { indent: ' ', enableColors: true}),
+        '/tmp/summary.html': htmlReport(data), 
+    }
+  }
